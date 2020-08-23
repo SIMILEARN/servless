93349020d40fbbbb2f7e76d4a602efcc2551110e
Yustres <58448153+Yustres@users.noreply.github.com>
2020-08-22 20:07:53 -0500

    nuevos

--- api/vistaTestDI.ts
+++ api/vistaTestDI.ts
@@ -4,4 +4,5 @@ import { mysql } from './lib/db';
 export default async function (req: NowRequest, res: NowResponse) {
   const preguntas = await mysql.query("SELECT * FROM preguntas_respuestas_testdi");
   res.json(preguntas);
+  console.log();
 }
