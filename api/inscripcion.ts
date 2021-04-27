fa425aea21bf750999414cc1ea03eb39a0fda952
Yustres <58448153+Yustres@users.noreply.github.com>
2021-04-26 20:44:00 -0500

    inscripcion terminada

--- api/inscripcion.ts
+++ api/inscripcion.ts
@@ -4,43 +4,14 @@ import { mysql } from './lib/db';
 
 export default async function (req: NowRequest, res: NowResponse) {
 
-    mysql.beginTransaction(function(err) {
-  if (err) { throw err; }
-  mysql.query(`INSERT INTO persona set ( nombre_persona= '${req.body.nombre_persona}',
-  edad_persona:${req.body.edad_persona},
-  imagen_usuario:'${req.body.imagen_usuario},
-  fk_rol : 1,
-  fk_ie:${req.body.fk_ie}`, function(err, result) {
+  let results = await mysql.transaction()
+  .query('INSERT INTO persona (nombre_persona,edad_persona,fk_rol,fk_ie) values (?,?,?,?)', [req.body.nombre_persona,req.body.edad_persona,req.body.fk_rol,req.body.fk_ie])
+  .query((r) => ['call registroUsuario(?,?,?,?)', [r.insertId, req.body.pin,req.body.usser,req.body.pasword]])
+  .rollback(e => { /* do something with the error */ }) // optional
+  .commit() 
 
-    if (err) { 
-      mysql.rollback(function() {
-        throw err;
-      });
-    }
- 
-    var log = result.id_persona;
- 
-    mysql.query(`call registroUsuario(${log},${req.body.pin}, ${req.body.usser}, ${req.body.pasword})`, function(err, result) {
-      if (err) { 
-        mysql.rollback(function() {
-          throw err;
-        });
-      }  
-      mysql.commit(function(err) {
-        if (err) { 
-          mysql.rollback(function() {
-            throw err;
-          });
-        }
-        console.log('Transaction Complete.');
-        mysql.end();
-      });
-    });
-  });
-});
-/* End transaction */
+
 
 
 }
-    
-  
+
