0c21bdd01dfdffa4306e9e38066afb037837219b
Yustres <58448153+Yustres@users.noreply.github.com>
2021-04-26 20:54:43 -0500

    inscripcion docente terminada

--- api/agregarDocente.ts
+++ api/agregarDocente.ts
@@ -6,7 +6,7 @@ export default async function (req: NowRequest, res: NowResponse) {
 
   let results = await mysql.transaction()
   .query('INSERT INTO persona (nombre_persona,edad_persona,fk_rol,fk_ie) values (?,?,?,?)', [req.body.nombre_persona,req.body.edad_persona,req.body.fk_rol,req.body.fk_ie])
-  .query((r) => ['call registroUsuario(?,?,?,?)', [r.insertId, req.body.pin,req.body.usser,req.body.pasword]])
+  .query((r) => ['insert into usuario (usser, pasword, fk_persona) values (?,?,?)', [req.body.usser,req.body.pasword, r.insertId]])
   .rollback(e => { /* do something with the error */ }) // optional
   .commit() 
 
