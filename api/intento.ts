6f380a6d9043d49e8e3675726d3018316255b621
Yustres <58448153+Yustres@users.noreply.github.com>
2021-04-29 02:31:27 -0500

    aun sin probar intento

--- api/intento.ts
+++ api/intento.ts
@@ -4,48 +4,19 @@ import { mysql } from './lib/db';
 
 export default async function (req: NowRequest, res: NowResponse) {
 
-  let datos ={
-    fk_test:req.body.fk_test,
-    fk_incrispcion:req.body.fk_incrispcion
+  let datos = {
+    fk_test: req.body.fk_test,
+    fk_incrispcion: req.body.fk_incrispcion
   }
- 
-  try {
-    await mysql.getConnection((err, conn) => {
-    
-      //Inicia la transacción
-      conn.beginTransaction( async function(err) {
-        if (err) { throw err; }
-        await conn.query("INSERT INTO intento set ?", [datos], async (err, result) => {
-          if (err) { 
-            conn.rollback(() => {
-              throw err;
-            });
-          }
-          var idintento = result.id;
 
-         Object.values(req.body.respuestas).map(async (respuesta) => {
-         const result = await conn.query(`CALL intento(${idintento}, ${respuesta})`, async (err, result) => {
-            if(err){
-              conn.rollback(() => {
-                throw err;
-              });
-            }
-          });
-          await conn.commit(async function (err) {
-            if (err) {
-              await conn.rollback(async function () {
-                throw err;
-              });
-            }
-            await conn.release();
-            res.json(result);
-          });
-        });
-        });
-      });
-    });
-  } catch (error) {
-    console.log(error);
-  } 
+  const intento = await mysql.query('call intento (?,?)', [req.body.idUsser, req.body.fkTest]);
+  
+  let results = await mysql.transaction()
+    .query('call insercionRespuestaEstudiante (?,?)', [req.body.idRes,req.body.idPre])
+    .query((r) => ['INSERT INTO desatalle_resultado (fk_intento, fk_respuesta) values (?,?)', [intento.insertId,r.insertId]])
+    .rollback(e => { /* do something with the error */ }) // optional
+    .commit()
+
+  res.json(results);
 
 }
