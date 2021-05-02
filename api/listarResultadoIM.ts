d7ec14a8405dcb22bcbefbe1e45934da9ba4ca12
Yustres <58448153+Yustres@users.noreply.github.com>
2021-05-02 03:22:32 -0500

    resultado im

--- api/listarResultadoIM.ts
+++ api/listarResultadoIM.ts
@@ -3,6 +3,12 @@ import { NowRequest, NowResponse } from '@vercel/node'
 import { mysql } from './lib/db';
 
 export default async function (req: NowRequest, res: NowResponse) {
-
+    try {
+        const result = await mysql.query(`call resultadoTest (${req.body.idIntento})`);
+        res.json(result);
+      } catch (error) {
+        console.log('Error =>', error);
+        res.send(error.sqlMessage);
+      }
 
 };
