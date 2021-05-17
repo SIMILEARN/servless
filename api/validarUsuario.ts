55c1fe1978c2f0fddaa81f5fa6e4103fd0b3991c
Yustres <58448153+Yustres@users.noreply.github.com>
2021-05-17 12:00:35 -0500

    validar usuario

--- api/validarUsuario.ts
+++ api/validarUsuario.ts
@@ -1,42 +1,59 @@
-import { NowRequest, NowResponse } from '@vercel/node'
+
 import { mysql } from './lib/db';
+
+
+import { NowRequest, NowResponse } from '@vercel/node';
+import escape from 'sql-template-strings';
+import allowCors from './lib/allowCors';
+import { sign } from 'jsonwebtoken';
 
 export default async function (req: NowRequest, res: NowResponse) {
 
-    var username = req.body.username,
-        password = req.body.password;
 
-    if (!username || !password) {
-        return res
-            .status(400)
-            .json({
-                error: "Needs a json body with { username: <username>, password: <password>}"
-            });
-    }
-
-    if (username !== password) {
-        return res
-            .status(401)
-            .json({
-                error: "Authentication failied.",
-            });
-    }
-
+const handler = async (req: NowRequest, res: NowResponse) => {
+  if(req.method === 'POST'){
     try {
-        const result = await mysql.query(`call procedure validarUsuario(${username},${password})`);
-        if (result == true){
-            res.json(result);
-        }else {
-            return res
-            .status(401)
-            .json({
-                error: "Authentication failied.",
-            });
-        }
-
+      req.body
+      const { user, password } = req.body;
+      if ((typeof user !== "undefined" && user) && (typeof password  !== "undefined" && password)) {
+        await mysql(escape`SELECT * FROM usuario WHERE username = ${user} AND password = ${password}`)
+        .then(response => {
+          if (response.length === 0){
+            errorCath(res, 401, 'El usuario no existe')
+          } else {
+            sign(response[0], 'privateKey', { expiresIn: 30 }, (err, token) => {
+              if(!err){
+                res.json({token:token})
+              } else {
+                console.log(err.toString())
+                errorCath(res, 500, null)
+              }
+            })
+          }
+        })
+      } else {
+        errorCath(res, 400, 'Usuario y contraseña son obligatorias')
+      }
     } catch (error) {
-        console.log('Error =>', error);
-        res.send(error.sqlMessage);
+      errorCath(res, 400, 'Las credenciales son incorrectas')
     }
+  } else {
+    res.status(405).send(null);
+  } 
+}
+
+const errorCath = (res, code, msg) => {
+  return (msg != null)
+  ? res.status(code).json({ error: msg })
+  : res.status(code).send(null)
+}
+
+module.exports = allowCors(handler)
+
+    
+ 
+
+
+
 };
 
